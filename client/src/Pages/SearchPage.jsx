fc41caf25717748b85e9980b1b861b1142e4f0a0
Piyachat Jongtong <wearingandtearing5221@gmail.com>
2024-06-19 17:11:35 +0700

    feat: add SearchContent component

--- client/src/Pages/SearchPage.jsx
+++ client/src/Pages/SearchPage.jsx
@@ -1,5 +1,20 @@
 function SearchPage() {
-  return <></>;
+  return (
+    <main className="main-container flex flex-row">
+      <div className="sidebar-container w-[25%] h-full flex flex-col gap-2">
+        <SideBar />
+        <MusicPlayer />
+      </div>
+      <div className="main-content-container w-[75%] h-screen">
+        <div className="header">
+          <Header />
+        </div>
+        <div className="content h-[85%] overflow-scroll overflow-x-hidden">
+          <Content />
+        </div>
+      </div>
+    </main>
+  );
 }
 
 export default SearchPage;
