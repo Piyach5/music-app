d1101a60e314ffa60be0e65525797db0157405a4
Piyachat Jongtong <wearingandtearing5221@gmail.com>
2024-06-20 13:39:23 +0700

    fix: add MusicPlayer component to sidebar

--- client/src/Components/SideBar.jsx
+++ client/src/Components/SideBar.jsx
@@ -3,24 +3,31 @@ import { library } from "@fortawesome/fontawesome-svg-core";
 import { faHouse, faSearch, faMusic } from "@fortawesome/free-solid-svg-icons";
 library.add(faHouse, faSearch, faMusic);
 import { useNavigate } from "react-router-dom";
+import MusicPlayer from "./MusicPlayer";
 
 function SideBar() {
   const navigate = useNavigate();
 
   return (
-    <div className="nav-bar flex flex-col justify-center gap-5 px-14 py-10 mx-2 mt-2  bg-cyan-950/40 rounded-md">
-      <h1>
-        <FontAwesomeIcon className="text-2xl mr-3" icon="fa-solid fa-music" />
-        MUSIC-APP
-      </h1>
-      <a onClick={() => navigate("/")}>
-        <FontAwesomeIcon className="text-2xl mr-3" icon="fa-solid fa-house" />
-        HOME
-      </a>
-      <a onClick={() => navigate("/search")}>
-        <FontAwesomeIcon className="text-2xl mr-3" icon="fa-solid fa-search" />
-        SEARCH
-      </a>
+    <div className="sidebar-container w-[25%] h-full flex flex-col gap-2">
+      <div className="nav-bar flex flex-col justify-center gap-5 px-14 py-10 mx-2 mt-2  bg-cyan-950/40 rounded-md">
+        <h1>
+          <FontAwesomeIcon className="text-2xl mr-3" icon="fa-solid fa-music" />
+          MUSIC-APP
+        </h1>
+        <a onClick={() => navigate("/")}>
+          <FontAwesomeIcon className="text-2xl mr-3" icon="fa-solid fa-house" />
+          HOME
+        </a>
+        <a onClick={() => navigate("/search")}>
+          <FontAwesomeIcon
+            className="text-2xl mr-3"
+            icon="fa-solid fa-search"
+          />
+          SEARCH
+        </a>
+      </div>
+      <MusicPlayer />
     </div>
   );
 }
--- client/src/Pages/HomePage.jsx
+++ client/src/Pages/HomePage.jsx
@@ -1,16 +1,12 @@
 import SideBar from "../Components/SideBar";
-import MusicPlayer from "../Components/MusicPlayer";
 import Header from "../Components/Header";
 import Content from "../Components/Content";
 
 function HomePage() {
   return (
-    <main className="main-container flex flex-row">
-      <div className="sidebar-container w-[25%] h-full flex flex-col gap-2">
-        <SideBar />
-        <MusicPlayer />
-      </div>
-      <div className="main-content-container w-[75%] h-screen">
+    <main className="flex flex-row">
+      <SideBar />
+      <div className="main-container w-[75%] h-screen">
         <div className="header">
           <Header />
         </div>
